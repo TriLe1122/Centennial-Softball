4f3290ba7300c5b00d6bb0726f65a890430e9a9a
tri le <letrin93@gmail.com>
2022-12-21 20:24:30 -0700

    tri

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,211 @@
+<template>
+
+
+    <div class="hero-image text-center d-flex align-items-center justify-content-center" >
+      <h1 class="text-center text-light  text-shadow font-1 page-title">Who We Are</h1>
+    </div>
+
+
+    <div class="container">
+      <div class="row">
+      <div class="col-md-12 my-5">
+  <!-- NavButtons -->
+  <nav class="" >
+    <div class="nav nav-tabs rounded justify-content-center justify-content-evenly" id="nav-tab" role="tablist">
+      <button class="nav-link active border-0 " id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home"
+        type="button" role="tab" aria-controls="nav-home" aria-selected="true">
+        <i class="mdi mdi-view-dashboard me-1"></i> DashBoard
+      </button>
+      <button class="nav-link  border-0 " id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile"
+        type="button" role="tab" aria-controls="nav-profile" aria-selected="false">
+       Dogs
+      </button>
+      <button class="nav-link  border-0 " id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact"
+        type="button" role="tab" aria-controls="nav-contact" aria-selected="false">
+        Cats
+      </button>
+
+      <button class="nav-link  border-0 " id="nav-fourth-tab" data-bs-toggle="tab" data-bs-target="#nav-fourth"
+        type="button" role="tab" aria-controls="nav-fourth" aria-selected="false">
+        Lizards
+      </button>
+    </div>
+  </nav>
+  <!-- !NavButtons -->
+  <div class="tab-content" id="nav-tabContent">
+    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
+      <!-- Tab1 -->
+   <div class="container-fluid ">
+     <div class="row">
+       <div class="col-md-12 my-3 p-5">
+      <p class="fs-4 ">   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid praesentium quaerat totam odit ea delectus est debitis obcaecati! Ex tempore doloribus laboriosam illum similique, earum nostrum cum labore rerum nam aliquid tempora dolor maxime sequi excepturi pariatur vel nobis rem ea magni quod repellendus dolorem perferendis. Delectus hic tempore eveniet beatae ratione porro voluptates dolorum rerum rem vitae incidunt natus est distinctio minima, adipisci suscipit deleniti architecto nostrum assumenda quae omnis atque animi dicta cumque. Voluptas voluptatum, sequi incidunt, enim perspiciatis ut labore accusantium dolore inventore, suscipit facilis dolor alias. Reiciendis eos possimus architecto eaque facilis quaerat ratione vitae error. Optio ab commodi, odit doloribus error sint et totam debitis veritatis perferendis. Deleniti, tempora assumenda. Itaque natus ullam consectetur optio! Voluptates corporis fugit incidunt ipsum, aperiam cumque nostrum ex facilis tempore dolores qui facere nesciunt aliquam mollitia porro deserunt repellat fugiat ut ea? Nisi at voluptates delectus corrupti eum ratione nesciunt eaque architecto tempore recusandae rerum consectetur tempora deleniti commodi vitae, alias suscipit aspernatur vel corporis! Eveniet delectus soluta libero nobis, error totam eligendi suscipit! Blanditiis velit perferendis nihil enim voluptatibus tenetur quibusdam architecto eos itaque alias. Sit voluptates consequuntur libero minus in odit, ea iusto eius nemo laborum.</p>
+       </div>
+     </div>
+   </div>
+      <!-- !Tab1 -->
+    </div>
+    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+      <!-- Tab2 -->
+  <div class="container">
+    <div class="row">
+     
+         <div class="col-md-12 my-3 p-5">
+      <p class="fs-4 ">  The dog (Canis familiaris[4][5] or Canis lupus familiaris[5]) is a domesticated descendant of the wolf. Also called the
+      domestic dog, it is derived from the extinct Pleistocene wolf,[6][7] and the modern wolf is the dog's nearest living
+      relative.[8] Dogs were the first species to be domesticated[9][8] by hunter-gatherers over 15,000 years ago[7] before
+      the development of agriculture.[1] Due to their long association with humans, dogs have expanded to a large number of
+      domestic individuals[10] and gained the ability to thrive on a starch-rich diet that would be inadequate for other
+      canids.[11]
+      
+      The dog has been selectively bred over millennia for various behaviors, sensory capabilities, and physical
+      attributes.[12] Dog breeds vary widely in shape, size, and color. They perform many roles for humans, such as hunting,
+      herding, pulling loads, protection, assisting police and the military, companionship, therapy, and aiding disabled
+      people. Over the millennia, dogs became uniquely adapted to human behavior, and the human-canine bond has been a topic
+      of frequent study.[13] This influence on human society has given them the sobriquet of "man's best friend".</p>
+       </div>
+    </div>
+  </div>
+      <!-- !Tab2 -->
+    </div>
+    <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab" tabindex="0">
+      <!-- Tab3 -->
+      <div class="container">
+        <div class="row">
+       
+           <div class="col-md-12 my-3 p-5">
+      <p class="fs-4 "> The cat (Felis catus) is a domestic species of small carnivorous mammal.[1][2] It is the only domesticated species in
+            the family Felidae and is commonly referred to as the domestic cat or house cat to distinguish it from the wild members
+            of the family.[4] Cats are commonly kept as house pets but can also be farm cats or feral cats; the feral cat ranges
+            freely and avoids human contact.[5] Domestic cats are valued by humans for companionship and their ability to kill
+            rodents. About 60 cat breeds are recognized by various cat registries.[6]
+            
+            The cat is similar in anatomy to the other felid species: it has a strong flexible body, quick reflexes, sharp teeth,
+            and retractable claws adapted to killing small prey. Its night vision and sense of smell are well developed. Cat
+            communication includes vocalizations like meowing, purring, trilling, hissing, growling, and grunting as well as
+            cat-specific body language. Although the cat is a social species, it is a solitary hunter. As a predator, it is
+            crepuscular, i.e. most active at dawn and dusk. It can hear sounds too faint or too high in frequency for human ears,
+            such as those made by mice and other small mammals.[7] It also secretes and perceives pheromones.[8]
+            
+            Female domestic cats can have kittens from spring to late autumn, with litter sizes often ranging from two to five
+            kittens.[9] Domestic cats are bred and shown at events as registered pedigreed cats, a hobby known as cat fancy.
+            Population control of cats may be effected by spaying and neutering, but their proliferation and the abandonment of pets
+            has resulted in large numbers of feral cats worldwide, contributing to the extinction of entire bird, mammal, and
+            reptile species.[10]
+            
+            It was long thought that cat domestication began in ancient Egypt, where cats were venerated from around 3100
+            BC,[11][12] but recent advances in archaeology and genetics have shown that their domestication occurred in Western Asia
+            around 7500 BC.[13]
+            
+            As of 2021, there were an estimated 220 million owned and 480 million stray cats in the world.[14][15] As of 2017, the
+            domestic cat was the second most popular pet in the United States, with 95.6 million cats owned[16][17][18] and around
+            42 million households owning at least one cat.[19] In the United Kingdom, 26% of adults have a cat, with an estimated
+            population of 10.9 million pet cats as of 2020</p>
+       </div>
+        </div>
+      </div>
+      <!-- !Tab3 -->
+    </div>
+    <div class="tab-pane fade" id="nav-fourth" role="tabpanel" aria-labelledby="nav-fourth-tab" tabindex="0">
+      <!-- Tab4 -->
+    <div class="container">
+      <div class="row">
+        
+          <div class="col-md-12 my-3 p-5">
+      <p class="fs-4 ">  Lizards are a widespread group of squamate reptiles, with over 7,000 species,[1] ranging across all continents except
+          Antarctica, as well as most oceanic island chains. The group is paraphyletic since it excludes the snakes and
+          Amphisbaenia although some lizards are more closely related to these two excluded groups than they are to other lizards.
+          Lizards range in size from chameleons and geckos a few centimeters long to the 3-meter-long Komodo dragon.
+          
+          Most lizards are quadrupedal, running with a strong side-to-side motion. Some lineages (known as "legless lizards"),
+          have secondarily lost their legs, and have long snake-like bodies. Some such as the forest-dwelling Draco lizards are
+          able to glide. They are often territorial, the males fighting off other males and signalling, often with bright colours,
+          to attract mates and to intimidate rivals. Lizards are mainly carnivorous, often being sit-and-wait predators; many
+          smaller species eat insects, while the Komodo eats mammals as big as water buffalo.
+          
+          Lizards make use of a variety of antipredator adaptations, including venom, camouflage, reflex bleeding, and the ability
+          to sacrifice and regrow their tails.</p>
+       </div>
+      </div>
+    </div>
+      <!-- !Tab4 -->
+    </div>
+  </div>
+</div>
+ 
+      </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+
+    }
+  }
+}
+</script>
+<style>
+
+.page-title{
+  font-size: 7rem;
+}
+
+
+nav .active {
+  border: 0;
+  border-bottom: 5px solid #f5a53c !important;
+  background-color: transparent !important;
+  font-weight: bold;
+}
+nav .nav-link {
+  color: black !important;
+  margin: 0 20px;
+  font-size: 1.5rem;
+  align-items: center;
+  display: flex;
+  border: none;
+}
+
+nav .nav-link:hover {
+  border: none;
+  border-bottom: #f5a53c 5px solid !important;
+}
+.navPanel {
+  height: 90vh;
+  width: auto;
+}
+
+.coosbay {
+  height: 15rem;
+  width: 15rem;
+}
+
+.navbg {
+  background-color: #6a7b76fd;
+}
+
+
+.twofiremen {
+  height: 16rem;
+  width: 16.5rem;
+}
+
+.navButton {
+  width: 15rem;
+}
+
+.hero-image {
+  background-image: url(https://images.unsplash.com/photo-1575920197509-2de60ffa1b0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1848&q=80);
+  height: 30vh;
+
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+}
+
+.randyPic {
+  height: 8rem;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,369 @@
+<template>
+  <div class="hero-image d-flex align-items-center justify-content-center">
+    <div class="col-md-12 text-center">
+      <h1 class="hero-text font-1 text-light px-5 text-shadow-2">
+        Randy Carpenter Memorial Foundation
+      </h1>
+    </div>
+  </div>
+  <div class="container-fluid mt-5">
+    <!-- section1 -->
+    <section>
+      <div class="row">
+        <div
+          class="col-md-5 d-flex align-items-center justify-content-center"
+         
+          
+        >
+          <img
+            src="https://scontent-fml2-1.xx.fbcdn.net/v/t1.18169-9/154320_466267117079_6118989_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=WDMrIYo_jxIAX-lfkcp&_nc_ht=scontent-fml2-1.xx&oh=00_AfDZpQIltUKPbi9QmLLR50ZFMIYJ2UPHLmUZEsWoVSae5w&oe=63B8D3E0"
+            alt=""
+            class="img-fluid image-2 hover-2 rounded elevation-6"
+          />
+        </div>
+        <div class="col-md-7" >
+          <div
+            class="card border-0 p-3 d-flex justify-content-around"
+           
+          >
+            <div class="d-md-flex">
+              <a href="#tournament" class="text-dark mx-auto">
+                <button class="btn p-3">Sign Up for Golf tournament</button>
+              </a>
+              <a href="#donate" class="text-dark">
+                <button class="btn p-3 my-3 my-md-0">Make a Donation</button>
+              </a>
+              <button
+                class="btn p-3"
+                data-bs-toggle="modal"
+                data-bs-target="#newsletterForm"
+              >
+                Sign up for NewsLetter
+              </button>
+            </div>
+          </div>
+
+          <div
+            class="card p-md-5 border-0 mt-5 roboto"
+            
+          >
+            <figure class="">
+              <!-- <p class="fs-2">
+              <i class="mdi mdi-format-quote-open"></i> PREPARATION is the
+              SEPARATION between being a good Firefighter - or becoming the
+              BEST <i class="mdi mdi-format-quote-close"></i>
+            </p> -->
+
+              <blockquote class="p-5">
+                <p class="fs-3 text-center">
+                  <b class=""> PREPARATION</b>
+                  is the <b>SEPARATION</b> between being a good Firefighter - or
+                  becoming the <b>BEST</b>
+                </p>
+                <figcaption class="blockquote-footer fs-5 mt-5 text-center">
+                  Captain Randy Carpenter
+                </figcaption>
+              </blockquote>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--! section1 -->
+
+    <!-- section2 -->
+    <section>
+      <div class="row justify-content-evenly my-5 bg-primary darken-10">
+        <div class="col-md-12 my-5">
+          <h2 class="display-4 font-1">Providing Firefighters With</h2>
+        </div>
+        <div class="col-md-3 my-3" >
+          <img
+            src="https://images.unsplash.com/photo-1633092228879-d6a88c22e7bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+            alt=""
+            class="three-images hover-2 img-fluid rounded"
+          />
+          <div class="my-3">
+            <p class="fs-3 mb-0 font-1">Fire Ground Training</p>
+            <p>Through Hands On Experience</p>
+          </div>
+        </div>
+        <div class="col-md-5 my-3" >
+          <img
+            src="https://scontent-fml2-1.xx.fbcdn.net/v/t31.18172-8/13483319_10154795226862080_1434242293369889234_o.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=ZiXSMZmPGfQAX8jdQEq&_nc_ht=scontent-fml2-1.xx&oh=00_AfCdtEC9PsQ9xnwgPpD-7l2-LxZ58X2XRdFom-CA0ILBPg&oe=63B0634F"
+            alt=""
+            class="three-images hover-2 img-fluid rounded elevation-6"
+          />
+          <div class="my-3">
+            <p class="fs-3 mb-0 font-1">Lorem ipsum dolor</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-3 my-3">
+          <img
+            src="https://scontent-fml2-1.xx.fbcdn.net/v/t31.18172-8/13495663_10154795225037080_7692198579030023972_o.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=zOC_iireDwkAX_XgKG1&_nc_ht=scontent-fml2-1.xx&oh=00_AfD4Xk6C_-Yxv8sB8rNXS5za9ukRueRvCs9eEdLOF43Tsg&oe=63B07750"
+            alt=""
+            class="three-images hover-2 img-fluid rounded elevation-6"
+          />
+          <div class="my-3">
+            <p class="fs-3 font-1 mb-0">Lorem ipsum dolor</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- !section2 -->
+
+    <!-- section3 -->
+    <section>
+      <div class="row justify-content-center">
+        <div class="col-md-12 text-center" >
+          <h3 class="font-1 display-3">Sponsors</h3>
+        </div>
+        <div class="col-md-8 mb-5" >
+          <div
+            id="carouselExampleControls"
+            class="carousel slide"
+            data-bs-ride="carousel"
+          >
+            <div class="carousel-inner elevation-6 rounded-1">
+              <div class="carousel-item active">
+                <img
+                  src="https://images.unsplash.com/photo-1524856781660-e5c92f4ac62a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                  class="d-block w-100"
+                  alt="..."
+                />
+              </div>
+              <div class="carousel-item">
+                <img
+                  src="https://images.unsplash.com/photo-1614338577197-5812cb856df7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80."
+                  class="d-block w-100"
+                  alt="..."
+                />
+              </div>
+              <div class="carousel-item">
+                <img
+                  src="https://images.unsplash.com/photo-1504667290505-eee11f23905a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                  class="d-block w-100"
+                  alt="..."
+                />
+              </div>
+            </div>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleControls"
+              data-bs-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleControls"
+              data-bs-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- !section3 -->
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+
+export default {
+  setup() {
+    onMounted(() => {
+      hideOnScrollTest();
+      // getFaceBookPictures();
+    });
+
+   
+
+    function hideOnScrollTest() {
+      let nav = document.querySelector("#searchBar22");
+      let prevScrollpos = window.scrollY;
+      // console.log(["prev"], prevScrollpos);
+      window.onscroll = function () {
+        let currentScrollPos = window.scrollY;
+        // console.log(["current"], currentScrollPos);
+        if (prevScrollpos > currentScrollPos) {
+          nav.style.top = "0";
+          nav.style.transition = "all 0.25s ease";
+          // nav.classList.remove("showOnScroll");
+        } else {
+          // nav.classList.add("hiddenOnScroll");
+          nav.style.top = "-85px";
+        }
+        prevScrollpos = currentScrollPos;
+      };
+    }
+
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.image-2 {
+  height: 600px;
+  object-fit: cover;
+}
+.hero-image {
+  height: 40vh;
+  /* always scale the image to the appropriate size of your screen */
+  background-size: cover;
+  background-image: url(https://images.unsplash.com/photo-1524856781660-e5c92f4ac62a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80);
+  background-position: center top;
+  /* keeps the image fixed while scrolling , neat effect. */
+  background-attachment: fixed;
+
+  //when screen is 768px OR LESS
+  @media only screen and (max-width: 768px) {
+    height: 30vh;
+  }
+  .hero-text {
+    font-size: 7rem;
+
+    //when screen is 768px OR LESS
+    @media only screen and (max-width: 768px) {
+      font-size: 3rem;
+    }
+  }
+}
+
+.three-images {
+  height: 30rem;
+  width: auto;
+  object-fit: cover;
+}
+#searchBar22 {
+  transition: all 0.5s ease;
+}
+
+.btn {
+  border: none;
+  display: block;
+  text-align: center;
+  cursor: pointer;
+  text-transform: uppercase;
+  outline: none;
+  overflow: hidden;
+  position: relative;
+  color: #fff;
+  font-weight: 700;
+  font-size: 15px;
+  background-color: #222;
+  padding: 17px 60px;
+  margin: 0 auto;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+  transition: all 0.5s ease;
+}
+.btn:hover {
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+    rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+}
+.btn span {
+  position: relative;
+  z-index: 1;
+}
+
+.btn:after {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 490%;
+  z-index: -1;
+  width: 140%;
+  background: #41ffc0e1;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+    rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+  -webkit-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
+  -webkit-transform: translateX(-98%) translateY(-25%) rotate(45deg);
+  transform: translateX(-98%) translateY(-25%) rotate(45deg);
+}
+
+.btn:hover:after {
+  -webkit-transform: translateX(-9%) translateY(-25%) rotate(45deg);
+  transform: translateX(-9%) translateY(-25%) rotate(45deg);
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+    rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+}
+
+blockquote {
+  position: relative;
+  margin: 40px 0;
+  border: 2px solid white;
+  box-shadow: 2px 2px 4px hsl(0 0% 0% / 20%);
+  text-indent: 1.6em;
+  transition: all 0.5s ease;
+}
+
+blockquote::before {
+  content: "";
+  pointer-events: none;
+  position: absolute;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  border-radius: var(--border-rad);
+  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12);
+}
+
+blockquote::after {
+  content: "❝";
+  position: absolute;
+  z-index: 1;
+  left: 50%;
+  top: -2px;
+  transform: translate(-50%, -50%);
+  width: 1.3em;
+  height: 1.3em;
+  background: white;
+  box-shadow: 0 4px 5px -1px hsla(0 0% 0% / 20%);
+  border-radius: 999px;
+  display: grid;
+  place-content: center;
+  padding-top: 0.5em;
+  color: var(--accent-color);
+  font-size: 36px;
+  font-style: normal;
+  text-indent: 0;
+}
+
+blockquote:hover {
+  transition: all 0.5s ease;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+    rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+}
+
+@media screen and (min-width: 768px) {
+  .hero-text {
+    font-size: 2rem;
+    color: #000;
+  }
+}
+</style>
