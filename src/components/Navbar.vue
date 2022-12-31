41d199432709ca4283181b791fe6e427af2309e5
molly-nettleton <mollyelainen@gmail.com>
2022-12-30 23:39:59 -0700

    dropdown

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,35 +2,31 @@
   <nav class="navbar navbar-expand-lg bg-primary py-5 rounded-4 mt-2 elevation-5 ">
     <img src="src\assets\img\12791.png" alt="" class="logo elevation-2 rounded-circle" style="width:9%">
     <h2 class="p-2 title text-primary bg-success rounded-pill px-3 fb ">CENTENNIAL HIGH SCHOOL</h2>
-    <!-- <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
-      </div>
-    </router-link> -->
-    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button> -->
+
     <div>
-      <div class="mt-md-5 pt-md-3 navbar-nav me-auto" id="navbarText">
+      <div class="mt-md-5 pt-md-3 me-auto navbar-nav">
         <!-- <ul class="navbar-nav me-auto">
           <li> -->
         <router-link :to="{ name: 'Home' }" class="btn text-light selectable text-uppercase">
           <h3>HOME</h3>
         </router-link>
-        <!-- </li>
-          <li> -->
         <router-link :to="{ name: 'About' }" class="btn text-light selectable text-uppercase">
           <h3>About</h3>
         </router-link>
-        <!-- </li>
-
-        </ul> -->
-        <!-- LOGIN COMPONENT HERE -->
-        <!-- <Login /> -->
       </div>
       <div>
-        <div class="bg-success font-primary px-3 my-2 d-flex gap-5 p-2 rounded-3 elevation-5 list">
+
+        <!-- <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+        <div class="d-flex flex-column align-items-center">
+          <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+        </div>
+      </router-link> -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+          aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="bg-success font-primary px-3 my-2 d-flex gap-5 p-2 rounded-3 elevation-5 list navbar-nav me-auto"
+          id="navbarText">
           <div class="rounded-2 fw-bold text-primary selectable text-uppercase">Schedule</div>
           <div class="rounded-2 fw-bold text-primary selectable text-uppercase">Coaches</div>
           <div class="rounded-2 fw-bold text-primary selectable text-uppercase">Roster</div>
